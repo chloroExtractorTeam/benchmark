52edfa0c9b588e49b592f5e140f15d72ef147a07
Frank Förster <frank.foerster@ime.fraunhofer.de>
2019-04-04 01:08:01 +0200

    Add source branch detection for build script

--- docker/generate_docker_images.sh
+++ docker/generate_docker_images.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+export SOURCE_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+# in case SOURCE_BRANCH is HEAD, we want to have the tag instead
+# taken from https://stackoverflow.com/questions/3404936/show-which-git-tag-you-are-on
+if [ ${SOURCE_BRANCH} = "HEAD" ]
+then
+    export SOURCE_BRANCH=$(git describe --tags)
+fi
 
 for i in benchmark-baseimage benchmark_chloroextractor benchmark_fast-plast benchmark_GetOrganelle benchmark_IOGA benchmark_NOVOPlasty benchmark_org-asm benchmark_chloroplast_assembly_protocol
 do
 	cd ${i};
 	export DOCKER_REPO=chloroextractorteam/$(echo $i | tr "A-Z" "a-z");
-	export IMAGE_NAME="${DOCKER_REPO}":docker
+	export IMAGE_NAME="${DOCKER_REPO}":${SOURCE_BRANCH}
 
 	./hooks/build &>/dev/zero
 
