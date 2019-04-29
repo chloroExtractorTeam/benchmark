09a27e2ee58c80c29a303ea56fc0558a18a22266
Markus Ankenbrand <markus@ankenbrand.me>
2019-04-29 17:00:35 +0200

    Add completeness evaluation script

---
+++ code/evaluate_completeness.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+REF=$1
+QRY=$2
+NAME=$3
+
+minimap2 -a -N 5 -x asm5 --cs -c $REF $QRY | samtools view -Sb | samtools sort >${NAME}_vs_ref.bam
+minimap2 -a -N 5 -x asm5 --cs -c $QRY $REF | samtools view -Sb | samtools sort >ref_vs_${NAME}.bam
+
+bedtools genomecov -ibam ${NAME}_vs_ref.bam -bga | perl -ane '$l=$F[2]-$F[1];$t+=$l;$c+=$l if($F[3]>0);END{print "Ref\t$t\t$c\t".($c/$t)."\n"}'
+bedtools genomecov -ibam ref_vs_${NAME}.bam -bga | perl -ane '$l=$F[2]-$F[1];$t+=$l;$c+=$l if($F[3]>0);END{print "Qry\t$t\t$c\t".($c/$t)."\n"}'
