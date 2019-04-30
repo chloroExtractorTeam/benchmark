852c4fb8be7994141937ba01069b516c6e5dae13
Joyvalley <j.a.freudenthal@gmail.com>
2019-04-30 09:07:18 +0200

    Update evaluate_completeness.sh

--- code/evaluate_completeness.sh
+++ code/evaluate_completeness.sh
@@ -7,8 +7,14 @@ NAME=$3
 minimap2 -a -N 5 -x asm5 --cs -c $REF $QRY | samtools view -Sb | samtools sort >${NAME}_vs_ref.bam
 minimap2 -a -N 5 -x asm5 --cs -c $QRY $REF | samtools view -Sb | samtools sort >ref_vs_${NAME}.bam
 
-bedtools genomecov -ibam ${NAME}_vs_ref.bam -bga | perl -ane '$l=$F[2]-$F[1];$t+=$l;$c+=$l if($F[3]>0);END{print "'$NAME'\t$t\t$c\t".($c/$t)}'
-bedtools genomecov -ibam ref_vs_${NAME}.bam -bga | perl -ane '$l=$F[2]-$F[1];$t+=$l;$c+=$l if($F[3]>0);END{print "\t$t\t$c\t".($c/$t)."\n"}'
+echo -ne "$NAME\t"
+bedtools genomecov -ibam ${NAME}_vs_ref.bam -bga | perl -ane '$l=$F[2]-$F[1];$t+=$l;$c+=$l if($F[3]>0);END{print "$t\t$c\t".($c/$t)}'
+bedtools genomecov -ibam ref_vs_${NAME}.bam -bga | perl -ane '$l=$F[2]-$F[1];$t+=$l;$c+=$l if($F[3]>0);END{print "\t$t\t$c\t".($c/$t)."\t"}'
+grep -c  "^>" $QRY
 
 rm ${NAME}_vs_ref.bam 
 rm ref_vs_${NAME}.bam
+
+
+#for i in */*/output.fa ; do echo -ne "$(dirname $(dirname $i))\t" ; ../evaluate_completeness.sh /home/maa62rb/sim/athaliana_chloro_sim/TAIR10_chrC.fas $i $(basename $(dirname $i)) >> res.tsv ; done
+
