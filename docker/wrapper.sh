a26ae0b20b11f5404dc1522ddde04add439d5f09
Frank Förster <frank.foerster@ime.fraunhofer.de>
2019-05-22 12:44:12 +0200

    Fix change of CPUTHREADS for CAP by rewriting the original perl script

--- docker/wrapper.sh
+++ docker/wrapper.sh
@@ -242,8 +242,6 @@ then
 
     0_get_cp_reads.pl input cp_noref $REFERENCE
 
-    # set threads
-    sed -i 's/\(CPUTHREADS[[:space:]]*=[[:space:]]*\)[0-9]*/\1'${NUMCPUS}'/g' $(which 1_cleanreads.pl)
     1_cleanreads.pl -folder cp_noref
 
     if [ -e cp_noref/cleanreads.txt ]
