c3c25bdebb3dc8814923ab954f18039262cec595
Jan Freudenthal <j.a.freudenthal@gmail.com>
2019-07-15 19:08:39 +0200

    Fix bug in rm fast-plast

--- code/run_array.sh
+++ code/run_array.sh
@@ -1,53 +1,55 @@
-#!/bin/bash 
+File Edit Options Buffers Tools Sh-Script Help                                                                                                                                                
+#!/bin/bash                                                                                                                                                                                   
 
 i=$SLURM_ARRAY_TASK_ID ;
-ID=$SLURM_ARRAY_TASK_ID ; 
+ID=$SLURM_ARRAY_TASK_ID ;
+
 
 echo $i ;
 
-## data is a file containing all the datasets
+
 A=($(cat data)) ;
 
 
-dat=($(echo "${A[@]:$i:1}")) ; 
+dat=($(echo "${A[@]:$i:1}")) ;
 
-echo $dat  ; 
+echo $dat  ;
 con=$1 ;
 
-echo $con ; 
+echo $con ;
 
-chmod u+x run_singularity.sh; 
+chmod u+x run_singularity.sh;
 
-cd $dat ; 
+cd $dat ;
 
-pwd ; 
+pwd ;
 sh ../run_singularity.sh $con  v2.0.5 ;
 
 
 if   [[ $con == chloroextractor ]]; then
-    rm -r chloroextractor/ptx/ ;
+│   rm -r chloroextractor/ptx/ ;
 elif [[ $con == chloroplast_assembly_protocol ]]; then
-    rm -r chloroplastassemblyprotocol/*fastq ;
-    rm -r chloroplastassemblyprotocol/cp_noref ;
-    rm -r chloroplastassemblyprotocol/input  ;
+│   rm -r chloroplastassemblyprotocol/*fastq ;
+│   rm -r chloroplastassemblyprotocol/cp_noref ;
+│   rm -r chloroplastassemblyprotocol/input  ;
 elif [[ $con == fastplast ]] ; then
-    rm -r fast*/ ; 
+│   rm -r fast*/fast*/ ;
 elif [[ $con == org-asm ]] ; then
-    rm -r org-asm/chloro*/ ; 
-elif [[ $con == getorganelle ]] ; then 
-    rm -r get_organelle/fil* ;
-    rm -r get_organelle/seed/ ;
-    rm -r get_organelle/emb* ;
+│   rm -r org-asm/chloro*/ ;
+elif [[ $con == getorganelle ]] ; then
+│   rm -r get_organelle/fil* ;
+│   rm -r get_organelle/seed/ ;
+│   rm -r get_organelle/emb* ;
 elif [[ $con == ioga ]] ; then
-    rm -r iogaIOGA/ ; 
+│   rm -r ioga/IOGA* ;
 elif [[ $con == novoplasty ]] ; then
-    rm novo_plasty/*txt ; 
-    rm novo_plasty/*fast ; 
+│   rm novo_plasty/*txt ;
+│   rm novo_plasty/*fast ;
 fi
 
 
 
-cd .. 
+cd ..
 
 cp slurm-${SLURM_ARRAY_JOB_ID}_${ID}.out $dat/$con.out
 
