c8c91d05d926afa24e2db7c054a99d7ff861525e
Jan Freudenthal <j.a.freudenthal@gmail.com>
2019-07-30 19:33:01 +0200

    Update eval_novel.sh

--- code/eval_novel.sh
+++ code/eval_novel.sh
@@ -3,10 +3,10 @@
 QRY=$1
 NAME=$2
 echo -ne "$NAME\t"
-
-blastn -query $QRY -subject $QRY -outfmt  '7 qseqid sseqid pident length qstart qend sstart send sstrand slen qlen' | grep minus | awk '$3 ~ "100.00" ' | awk '$1 == $2' |   sort -nr -k 4,4 | sed -n 1p | awk  '{print $4, $11}' | tr "\n" "\t"
+###
+blastn -query $QRY -subject $QRY -outfmt  '7 qseqid sseqid pident length qstart qend sstart send sstrand slen qlen' | grep minus | awk  '$3 > 99.00 ' | awk '$1 == $2' |   sort -nr -k 4,4 | sed -n 1p | cut -f 4,11 | tr "\n" "\t"
 
 grep -c  "^>" $QRY
 
-# Execute with : 
-# for i in */*/output.fa ; do echo $i ;  echo -ne "$(dirname $(dirname $i))\t"  >> res.tsv ; ./eval_novel.sh  $i $(basename $(dirname $i)) >> res.tsv ; done
+#####
+for i in ERR1449077/*/output.fa ; do echo $i ;  echo -ne "$(dirname $(dirname $i))\t"  >> res_ERR.tsv ; ../novel/eval_novel.sh  $i $(basename $(dirname $i)) >> res_ERR.tsv ; done
